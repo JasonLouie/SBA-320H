0ee7e0188302f3d9659e6718cc0ae763bbdb0edb
Jason Louie <jason.louie29@myhunter.cuny.edu>
2025-11-16 20:29:51 -0500

    Improved styling for signup login forms. Added terms of use and privacy policy text.

--- src/components/Legal.jsx
+++ src/components/Legal.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import Button from "./Button";
 import Overlay from "./Overlay";
 
+const terms = "This Service is a non-commercial portfolio project created for demonstration purposes. It allows users to browse manga-related information provided by the public Jikan API. All data, images, and synopses are provided by the Jikan API. The Service does not own and is not responsible for the accuracy, availability, or content of this third-party data. All content belongs to its respective copyright holders.";
+const privacy = "This application is a front-end-only project. There is no backend server or database. Personal information is not collectd, stored, or shared on any server. When you create an account, your username, email, and list of favorited manga are stored in a file in your web browser's storage on your own device. For your safety, please do not use a real-world password that you use for any other service.";
+
 export default function Legal() {
 
     const [hidden, setHidden] = useState(true);
@@ -11,11 +14,7 @@ export default function Legal() {
     function showOverlay(e) {
         const btnText = e.target.textContent;
         setTitle(btnText);
-        if (btnText === "Terms of Use") {
-            setText("Some terms stuff");
-        } else {
-            setText("Some privacy stuff");
-        }
+        setText(btnText === "Terms of Use" ? terms : privacy);
         setHidden(false);
     }
 
